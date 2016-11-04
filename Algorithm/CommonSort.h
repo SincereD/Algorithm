59ad34b2c82fd8e355627e191210210f238add07
杜凯强 <kqdu@yellfun.com>
2016-11-04 09:37:24 +0800

    添加了一些注释

--- Algorithm/CommonSort.h
+++ Algorithm/CommonSort.h
@@ -8,16 +8,41 @@
 
 #import <Foundation/Foundation.h>
 
+//随机数个数
 #define DataCount 10
 
+//随机数范围
 #define DataRange 100
 
+/**
+ 排序基类
+ */
 @interface CommonSort : NSObject
 
+
+/**
+ 操作记录
+ */
 @property (nonatomic,retain) NSMutableArray * sortRecord;
 
+
+/**
+ 生成随机数据源
+
+ @return NSMutableArray
+ */
 - (NSMutableArray*)getRandomDataSource;
 
+
+/**
+ 保存每次执行排序操作的过程，用于执行动画、
+ 这里的无所谓From 和 To，最终结果都是交换值，视图x轴位置切换
+
+ @param i from
+ @param j to
+
+ @return NSDictionary
+ */
 - (NSDictionary*)getRemoveRecordWithI:(NSInteger)i J:(NSInteger)j;
 
 @end
--- Algorithm/SortView.h
+++ Algorithm/SortView.h
@@ -8,10 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ 排序视图
+ */
 @interface SortView : UIView
 
+
+/**
+ 根据首次数据初始化
+
+ @param numArr 随机数组
+
+ @return instancetype
+ */
 - (instancetype)initWithArray:(NSArray*)numArr;
 
+
+/**
+ 开始执行动画
+
+ @param recordArr 操作记录
+ */
 - (void)starAnimationWithArray:(NSArray*)recordArr;
 
 @end
